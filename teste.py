822a041d253d5cc50471f155504032c5f3a40c81
Sofia Domingues <124214443+SofiaDomeli@users.noreply.github.com>
2024-06-23 17:24:09 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -24,32 +24,4 @@ hora = str(data_ori.strftime('%H:%M'))
 
 driver.quit()
 
-# Conexão com o database 
-
-conn = psycopg2.connect(
-    dbname="defaultdb",
-    user="avnadmin",
-    password="AVNS_PiPCdU87ySKStvr7N8q",
-    host="pg-362da2a-wowk.f.aivencloud.com"
-)
- 
-# Criar um cursor
-cur = conn.cursor()
- 
-# Comando SQL para chamar a stored procedure
-comando = "CALL inserir_tabela(%s, %s, %s)"
- 
-# Argumentos para a stored procedure
-args = (data, hora, dolar)
- 
-# Executar a stored procedure
-cur.execute(comando, args)
- 
-# Confirmar a transação
-conn.commit()
- 
-# Fechar a conexão
-cur.close()
-conn.close()
-
 print(dolar)
